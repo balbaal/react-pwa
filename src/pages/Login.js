723b9e67e129e1a474c700fe60717841e77de36f
Balbaal <iqbal.mi20@gmail.com>
2020-09-15 03:01:19 +0800

    add: login page hero section

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,21 @@
 import React from "react";
+import Logo from "logo.svg";
 
 class Login extends React.Component {
   render() {
-    return <h1>Login Page</h1>;
+    return (
+      <div className="max-w-screen-sm mx-auto">
+        <section className="flex flex-col items-center justify-center text-center">
+          <img src={Logo} width={250} />
+          <h1 className="text-gray-700 font-light text-2xl leading-loose">Authenticator</h1>
+          <p className="text-gray-500 font-light text-xs leading-tight">
+            Protect your account in just a few minutes <br /> by reviewing
+            security settings <br /> and activity.
+          </p>
+        </section>
+        <section>login form</section>
+      </div>
+    );
   }
 }
 
