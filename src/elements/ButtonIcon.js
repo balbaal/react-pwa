2185a16c03591be96424f22e21018a75c2a5b4cc
Balbaal <iqbal.mi20@gmail.com>
2020-09-15 09:30:39 +0800

    fix: redirect link to other page

--- src/elements/ButtonIcon.js
+++ src/elements/ButtonIcon.js
@@ -2,10 +2,14 @@ import React from "react";
 
 const ButtonIcon = (props) => {
   const { icon, title, isActive } = props;
-  const classNames = "flex flex-col items-center justify-center text-gray-600";
+  const classNames =
+    "flex flex-col items-center justify-center text-gray-600 cursor-pointer";
 
   return (
-    <div className={[classNames, isActive ? "text-green-700" : null].join(" ")}>
+    <div
+      onClick={props.onClick}
+      className={[classNames, isActive ? "text-green-700" : null].join(" ")}
+    >
       {icon}
       <h6 className="mt-1" style={{ fontSize: 11 }}>
         {title}
--- src/parts/BottomNav.js
+++ src/parts/BottomNav.js
@@ -6,6 +6,7 @@ import { ButtonIcon } from "elements";
 
 const BottomNav = (props) => {
   const { path } = props.match;
+  const { push } = props.history;
 
   const checkingCurrentPage = (page) => {
     if (page === path) {
@@ -15,22 +16,27 @@ const BottomNav = (props) => {
     }
   };
 
+  console.log("props :>> ", props);
+
   return (
     <div className="flex items-center justify-between px-3 pb-1 pt-3 max-w-screen-sm mx-auto">
       <ButtonIcon
         icon={<i className="fa fa-home fa-lg"></i>}
         title="Home"
         isActive={checkingCurrentPage("/")}
+        onClick={() => push("/")}
       />
       <ButtonIcon
         icon={<i className="fa fa-plus-circle fa-lg"></i>}
         title="Sell"
         isActive={checkingCurrentPage("/sell")}
+        onClick={() => push("/sell")}
       />
       <ButtonIcon
         icon={<i className="fa fa-bell fa-lg"></i>}
         title="Activity"
         isActive={checkingCurrentPage("/activity")}
+        onClick={() => push("/activity")}
       />
     </div>
   );
